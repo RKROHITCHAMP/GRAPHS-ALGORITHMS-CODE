340ff9f3861508edc3679be9acbba1312406c678
ROHIT KUMAR <RK8826177@GMAIL.COM>
2022-12-17 20:05:01 +0530

    Update kosa_raju_algo.cpp

--- kosa_raju_algo.cpp
+++ kosa_raju_algo.cpp
@@ -1,92 +1,91 @@
-#include <bits/stdc++.h>
+//{ Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<int> *edges, int start, unordered_set<int> &visited, stack<int> &finished)
-{
-    visited.insert(start);
-    for (int i = 0; i < edges[start].size(); i++)
-    {
-        int adjacent = edges[start][i];
-        if (visited.count(adjacent) == 0)
-        {
-            dfs(edges, adjacent, visited, finished);
-        }
-    }
-    finished.push(start);
-}
-void dfs2(vector<int> *edges, int start, unordered_set<int> *component, unordered_set<int> &visited)
-{
-    visited.insert(start);
-    component->insert(start);
-    for (int i = 0; i < edges[start].size(); i++)
-    {
-        int adjacent = edges[start][i];
-        if (visited.count(adjacent) == 0)
-        {
-            dfs2(edges, adjacent, component, visited);
-        }
-    }
-}
+// } Driver Code Ends
 
-unordered_set<unordered_set<int> *> *getscc(vector<int> *edges, vector<int> *edgesT, int n)
+
+class Solution
 {
-    unordered_set<int> visited;
-    stack<int> finishvertex;
-    for (int i = 0; i < n; i++)
+	public:
+	//Function to find number of strongly connected components in the graph.
+    void dfs(int i,vector<int> adj[],vector<int> &vis,stack<int> &s)
+	{
+	    vis[i]=true;
+	    for(auto it : adj[i])
+	      if(!vis[it])
+	        dfs(it,adj,vis,s);
+	   s.push(i);
+	    
+	}
+	void dfs2(int i,vector<int> adj[],vector<int> &vis)
+	{
+	    vis[i]=true;
+	    for(auto it : adj[i])
+	      if(!vis[it])
+	        dfs2(it,adj,vis);
+	}
+    int kosaraju(int n, vector<int> adj[])
     {
-        if (visited.count(i) == 0)
-        {
-            dfs(edges, i, visited, finishvertex);
-        }
+        vector<int> vis(n,0);
+	    stack<int> s;
+	    for(int i=0;i<n;i++)
+	      if(!vis[i])
+	        dfs(i,adj,vis,s);
+	    int ans=0; 
+	    vector<int> adj2[n];
+	    for(int i=0;i<n;i++)
+	      {
+	          vis[i]=0;
+	          for(auto it : adj[i])
+	          {
+	              adj2[it].push_back(i);
+	          }
+	      }
+	      
+	    while(!s.empty())
+	      {
+	          auto curr = s.top();
+	          s.pop();
+	          if(!vis[curr])
+	           {
+	               ans++;
+	               dfs2(curr,adj2,vis);
+	           }
+	          
+	      }
+	    return ans;
     }
-    visited.clear();
-    unordered_set<unordered_set<int> *> *output = new unordered_set<unordered_set<int> *>();
-    while (finishvertex.size() != 0)
-    {
-        int element = finishvertex.top();
-        finishvertex.pop();
-        if (visited.count(element) != 0)
-        {
-            continue;
-        }
-        unordered_set<int> *component = new unordered_set<int>();
-        dfs2(edgesT, element, component, visited);
-        output->insert(component);
-    }
-    return output;
-}
+};
+
+//{ Driver Code Starts.
+
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<int> *edges = new vector<int>[n];
-    vector<int> *edgesT = new vector<int>[n];
-    for (int i = 0; i < m; i++)
+    
+    int t;
+    cin >> t;
+    while(t--)
     {
-        int j, k;
-        cin >> j >> k;
-        edges[j - 1].push_back(k - 1);
-        edgesT[k - 1].push_back(j - 1);
-    }
-    unordered_set<unordered_set<int> *> *components = getscc(edges, edgesT, n);
-    unordered_set<unordered_set<int> *>::iterator it1 = components->begin();
-    while (it1 != components->end())
-    {
-        unordered_set<int> *component = *it1;
-        unordered_set<int>::iterator it2 = component->begin();
-        while (it2 != component->end())
-        {
-            cout << *it2 + 1 << " ";
-            it2++;
-        }
-        it1++;
-        cout << endl;
-    }
+    	int V, E;
+    	cin >> V >> E;
 
-    delete components;
-    delete[] edges;
-    delete[] edgesT;
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
 
     return 0;
 }
+
+
+// } Driver Code Ends
